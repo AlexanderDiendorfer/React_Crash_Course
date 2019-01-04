cbe450682a328c4c7b21f48ecfc11a35de7098c7
Alexander Diendorfer+ <20122072@htlneufelden.onmicrosoft.comgit config --global user.email 20122072@htlneufelden.onmicrosoft.comgit config --global user.email 20122072@htlneufelden.onmicrosoft.com>
2019-01-04 21:36:29 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+// import React from 'react';      other way to import React Component
+import './App.css';
+
+// class App extends React.Component {      other way to import React Component
+class App extends Component {
+	state = {
+		todos: [
+			{
+				id: 1,
+				title: 'Take out the trash',
+				completed: false
+			},
+			{
+				id: 2,
+				title: 'Dinner with wife',
+				completed: true
+			},
+			{
+				id: 3,
+				title: 'Meeting with boss',
+				completed: false
+			}
+		]
+	};
+	render() {
+		return (
+			<div className="App">
+				<Todos todos={this.state.todos} />
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Todos from './Todos';
+
+export class TodoItem extends Component {
+	getStyle = () => {
+		return {
+			background: '#f4f4f4',
+			padding: '10px',
+			borderBottom: '1px #ccc dotted',
+			textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+		};
+		// if (this.props.todo.completed) {
+		// 	return {
+		// 		textDecoration: 'line-through'
+		// 	};
+		// } else {
+		// 	return {
+		// 		textDecoration: 'none'
+		// 	};
+		// }
+	};
+
+	render() {
+		return (
+			// <div style={{ backgroundColor: '#f4f4f4' }}>
+			// <div style={itemStyle}>
+			<div style={this.getStyle()}>
+				<p>{this.props.todo.title}</p>
+			</div>
+		);
+	}
+}
+
+// PropTypes
+TodoItem.propTypes = {
+	todo: PropTypes.object.isRequired
+};
+
+// const itemStyle = {
+// 	backgroundColor: '#f4f4f4'
+// };
+
+export default TodoItem;
